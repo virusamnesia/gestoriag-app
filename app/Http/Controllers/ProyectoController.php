918f45b9355d434d8f2cd9939e34e9fd347df63d
virusamnesia <62041438+virusamnesia@users.noreply.github.com>
2025-01-24 23:01:20 -0600

    redirección a la vista de terminos

--- app/Http/Controllers/ProyectoController.php
+++ app/Http/Controllers/ProyectoController.php
@@ -231,7 +231,7 @@ class ProyectoController extends Controller
                 ->select('terminos_pago_clientes.*')
                 ->get();
             $inf = 1;
-            return redirect()->route('terminos.proyectos',['cliente' => $cliente,'productos' => $productos, 'id' => $id,'terminos' => $terminos])->with('info',$inf);
+            return redirect()->view('proyecto.terminos',['cliente' => $cliente,'productos' => $productos, 'id' => $id,'terminos' => $terminos])->with('info',$inf);
 
         }
         else{
