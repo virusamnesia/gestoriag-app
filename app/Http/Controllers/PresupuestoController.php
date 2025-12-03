ce2400da9f2f5f2867d163c9089807fbc9bbeeba
virusamnesia <62041438+virusamnesia@users.noreply.github.com>
2025-12-02 20:22:07 -0600

    sin valores en el mensaje de update linea

--- app/Http/Controllers/PresupuestoController.php
+++ app/Http/Controllers/PresupuestoController.php
@@ -599,7 +599,7 @@ class PresupuestoController extends Controller
                 ->where('id','=', $id)
                 ->update($data);
 
-            $inf = $request->costo.'El costo se modificó con éxito...'.$request->cant;
+            $inf = 'El costo se modificó con éxito...';
             session()->flash('Exito',$inf);
             return redirect()->route('presupuestos.lineas', ['id' => $id])->with('message',$inf);   
         }
