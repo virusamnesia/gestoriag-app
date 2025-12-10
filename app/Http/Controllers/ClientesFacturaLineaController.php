6bddf8a527e1fbaf7f16eac1364b36affd68e12e
virusamnesia <62041438+virusamnesia@users.noreply.github.com>
2025-12-09 20:39:38 -0600

    prueba

--- app/Http/Controllers/ClientesFacturaLineaController.php
+++ app/Http/Controllers/ClientesFacturaLineaController.php
@@ -48,7 +48,7 @@ class ClientesFacturaLineaController extends Controller
         $proyecto_id = $proyecto->id;
         $cliente = $cliente->nombre;
         $proyecto = $proyecto->nombre;
-        $subtotal = $proyecto['cxc'];
+        $subtotal = 0 + $proyecto->cxc;
 
         $inf = 1;
         session()->flash('Exito','Selecciona las partidas a facturar...');
