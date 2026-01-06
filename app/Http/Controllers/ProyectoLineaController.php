a2902bf4df600ec065ad5e85076fc11a918d7218
virusamnesia <62041438+virusamnesia@users.noreply.github.com>
2026-01-06 11:39:56 -0600

    lefjoin en proyectos lineas con presupuestos

--- app/Http/Controllers/ProyectoLineaController.php
+++ app/Http/Controllers/ProyectoLineaController.php
@@ -42,7 +42,7 @@ class ProyectoLineaController extends Controller
         ->leftJoin('estatus_linea_clientes', 'estatus_linea_clientes.id', '=', 'proyecto_lineas.estatus_linea_cliente_id')
         ->leftjoin('tipos_productos', 'tipos_productos.id', '=', 'productos.tipos_producto_id')
         ->leftJoin('estatus_linea_proveedors', 'estatus_linea_proveedors.id', '=', 'proyecto_lineas.estatus_linea_proveedor_id')
-        ->join('presupuestos', 'presupuestos.id', '=', 'proyecto_lineas.presupuesto_id')
+        ->leftjoin('presupuestos', 'presupuestos.id', '=', 'proyecto_lineas.presupuesto_id')
         ->select('proyecto_lineas.*','sucursals.nombre as sucursal','sucursals.domicilio as domicilio',
         'municipio_contactos.nombre as municipio', 'estado_contactos.alias as estado', 'pais_contactos.alias as pais','proyectos.id as proyecto_id',
         'productos.id as producto_id', 'productos.nombre as producto', 'terminos_pago_clientes.nombre as terminos','estatus_linea_clientes.nombre as estatus',
